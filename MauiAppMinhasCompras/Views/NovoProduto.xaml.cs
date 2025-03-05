4ee6fb3f8f8073d7cf754a4a32e9ffc528ba2fe5
rafabarbosasilva <rafabarbosasilva@gmail.com>
2025-03-05 14:58:43 -0300

    alterações na tela inicial

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,35 @@
+
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
 {
-	public NovoProduto()
-	{
-		InitializeComponent();
-	}
+    public NovoProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Insert(p);
+
+            await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
