01c72ef1291d9a54d3dd81e2225b3bdd10f43587
rafabarbosasilva <rafabarbosasilva@gmail.com>
2025-03-23 09:29:35 -0300

    correções dos bugs

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,9 +4,23 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descriao;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao { 
+            get => _descriao; 
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descriao = value;
+            }
+        }
+
         public double Quantidade { get; set; }
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
